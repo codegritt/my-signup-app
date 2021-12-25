edc45cfd6cc850a8152e3e734bc20c5613d69e1f
Gokul <gokulchandan18@gmail.com>
2021-12-25 13:53:27 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from './shared/api.service';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule(
   {
     declarations: [
       AppComponent,
-      EmployeeDashboardComponent
+      EmployeeDashboardComponent,
+      LoginComponent,
+      SignupComponent
      
     ],
     imports: [
