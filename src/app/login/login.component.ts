e9ffb16afbbdefeb785a6896c798723c7825e668
Gokul <gokulchandan18@gmail.com>
2021-12-25 14:33:08 +0530

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, } from '@angular/core';
+import {FormGroup,FormBuilder, Validators} from '@angular/forms'
+import { Router } from '@angular/router';
+import { ApiService } from '../shared/api.service';
+import { UserModel } from '../shared/model/user.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm !: FormGroup;
+  public loginObj = new UserModel();
+  constructor(private fb :FormBuilder, private http : HttpClient,private router : Router,private api : ApiService) { }
+
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email:["",Validators.compose([Validators.required,Validators.email])],
+      password:["",Validators.required]
+    });
+   localStorage.clear();
   }
+  login(){
+  //   this.http.get<any>("http://localhost:3000/signupUsers")
+  //   .subscribe(res=>{
+  //     const user = res.find((a:any)=>{
+  //       return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+  //     });
+  //     if(user){
+  //       alert("Login Success!!");
+  //       this.router.navigate(['dashboard']);
+  //         this.loginForm.reset();
+  //     }
+  //   },err=>{
+  //     alert("Something went wrong!!")
+  //   })
+  this.loginObj.UserName = this.loginForm.value.email;
+  this.loginObj.Password = this.loginForm.value.password;
+  this.api.login(this.loginObj)
+  .subscribe(res=>{
+    alert(res.message);
+    this.router.navigate(['dashboard']);
+    localStorage.setItem('token',res.token);
+    localStorage.setItem('userType',res.userType);
+  },err=>{
+    alert("soomething went wrong")
+  })
+   }
 
 }
